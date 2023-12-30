57d0b4daf7ed4208eea626dc65d9aff1065d1215
Hüseyin Cenik <127469334+huseyincenik@users.noreply.github.com>
2023-12-31 00:48:08 +0300

    Add files via upload

--- Project/auto_analytics_advanced_estimation_&_deployment/auto_price_streamlit_web_site.py
+++ Project/auto_analytics_advanced_estimation_&_deployment/auto_price_streamlit_web_site.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import os
+from PIL import Image
+import pickle
 
 st.markdown("---")
 
@@ -14,6 +17,12 @@ data_button_key = "data_button"  # Unique key
 if data_button.button("Data", key=data_button_key):
     st.session_state.data_button_clicked = not st.session_state.data_button_clicked
 
+# Initialize 'info_button_clicked' and 'links_button_clicked' if it's the first run
+if 'info_button_clicked' not in st.session_state:
+    st.session_state.info_button_clicked = False
+if 'links_button_clicked' not in st.session_state:
+    st.session_state.links_button_clicked = False
+
 # HTML template
 html_temp = """
 <div style="background-color:tomato;padding:10px">
@@ -26,6 +35,9 @@ st.markdown("")
 if st.session_state.data_button_clicked:
     # Data descriptions
     data_descriptions = {
+        'make_model': 'Car model',
+        'gearbox': 'Gearbox type',
+        'drivetrain': 'Drivetrain type',
         'power_kW': 'Engine power (kW)',
         'age': 'Age',
         'empty_weight': 'Empty weight',
@@ -33,9 +45,6 @@ if st.session_state.data_button_clicked:
         'gears': 'Number of gears',
         'cons_avg': 'Average consumption',
         'co_emissions': 'CO emissions',
-        'make_model': 'Car model',
-        'gearbox': 'Gearbox type',
-        'drivetrain': 'Drivetrain type',
     }
 
     # Data as a DataFrame
@@ -46,24 +55,20 @@ if st.session_state.data_button_clicked:
     st.dataframe(data_df)
     st.markdown("\n\n")
 
-# İlk çalıştırma olduğunda session_state'i başlat
-if 'button_clicked' not in st.session_state:
-    st.session_state.button_clicked = False
-
-# Butonlar
+# Buttons
 info_button, links_button, _ = st.columns(3)
 
-# Information butonu
-info_button_key = "info_button"  # Benzersiz anahtar
+# Information button
+info_button_key = "info_button"  # Unique key
 if info_button.button("Information", key=info_button_key):
     st.session_state.info_button_clicked = not st.session_state.info_button_clicked
 
-# Links butonu
-links_button_key = "links_button"  # Benzersiz anahtar
+# Links button
+links_button_key = "links_button"  # Unique key
 if links_button.button("Links", key=links_button_key):
     st.session_state.links_button_clicked = not st.session_state.links_button_clicked
 
-# Bilgi markdown'ı
+# Information markdown
 info_markdown = """
 This project revolves around using machine learning algorithms to estimate car prices. 
 
@@ -73,7 +78,7 @@ The following regression algorithms were implemented:
 - Ridge Regression
 - Decision Tree
 - Random Forest
-- XGBoost (using GridSearchCV)
+- XGBoost
 
 Model evaluation, grid-search and cross-validation were performed, resulting in the following scores:
 
@@ -86,6 +91,8 @@ Model evaluation, grid-search and cross-validation were performed, resulting in 
 | ElasticNet        | 0.830 | 2817.18| 4959.12| 0.192  |
 | Decision Tree     | 0.816 | 3467.44| 5157.75| 0.221  |
 """
+
+# Links HTML
 links_html = """
 <div style="margin-bottom: 20px;">
     <h3 style="background-color: #FF6961; color: white; padding: 10px; border-radius: 5px;">🚗📈 Auto Analytics: Advanced Estimation & Deployment 🛠️</h3>
@@ -120,16 +127,15 @@ links_html = """
     </a>
 </div>
 """
-# Links gösterimi
+
+# Show Information
 if st.session_state.info_button_clicked:
-    # Links HTML'ı
     st.markdown("---")
     st.markdown(f"<div style='text-align: center; padding: 10px; border: 1px solid #e0e0e0; border-radius: 10px;'>{info_markdown}</div>", unsafe_allow_html=True)
     st.markdown("\n\n")
 
-# Links gösterimi
+# Show Links
 if st.session_state.links_button_clicked:
-    # Links HTML'ı
     st.markdown("---")
     st.markdown(f"<div style='text-align: center; padding: 10px; border: 1px solid #e0e0e0; border-radius: 10px;'>{links_html}</div>", unsafe_allow_html=True)
     st.markdown("\n\n")
@@ -168,25 +174,27 @@ st.markdown("---")
 st.write(df.sample(5))
 
 
-make_model = st.sidebar.selectbox("Select the Auto Brand - Model", df["make_model"].unique(), index = 28)
-gearbox = st.sidebar.selectbox("Select the Gearbox", df["gearbox"].unique(), index = 1)
-drivetrain = st.sidebar.selectbox("Select the Drivetrain", df["drivetrain"].unique(), index = 1)
-power_kw = st.sidebar.number_input("Enter the power (in kW)", min_value = df["power_kW"].min(), max_value = df["power_kW"].max(), value = df["power_kW"].mode().iloc[0])
-age = st.sidebar.number_input("Enter the Age", min_value = df["age"].min(), max_value = df["age"].max(), value = df["age"].mode().iloc[0])
-empty_weight = st.sidebar.number_input("Enter the Empty Weight", min_value = df["empty_weight"].min(), max_value = df["empty_weight"].max(), value = df["empty_weight"].mode().iloc[0])
-mileage = st.sidebar.number_input("Enter the Mileage", min_value = df["mileage"].min(), max_value = df["mileage"].max(), value = df["mileage"].mode().iloc[0])
-gears = st.sidebar.number_input("Enter the Gears", min_value = df["gears"].min(), max_value = df["gears"].max(), value = df["gears"].mode().iloc[0])
-cons_avg = st.sidebar.number_input("Enter the Average Consumption", min_value = df["cons_avg"].min(), max_value = df["cons_avg"].max(), value = df["cons_avg"].mode().iloc[0])
-co_emissions = st.sidebar.number_input("Enter the Average CO Emissions", min_value = df["co_emissions"].min(), max_value = df["co_emissions"].max(), value = df["co_emissions"].mode().iloc[0])
+make_model = st.sidebar.selectbox("Select Auto Brand - Model", df["make_model"].unique(), index = 28)
+gearbox = st.sidebar.selectbox("Select Gearbox", df["gearbox"].unique(), index = 1)
+drivetrain = st.sidebar.selectbox("Select Drivetrain", df["drivetrain"].unique(), index = 1)
+power_kw = st.sidebar.number_input("Enter Power (in kW)", min_value = df["power_kW"].min(), max_value = df["power_kW"].max(), value = df["power_kW"].mode().iloc[0], step = 1.0)
+age = st.sidebar.number_input("Enter Age", min_value = df["age"].min(), max_value = df["age"].max(), value = df["age"].mode().iloc[0], step = 1.0)
+empty_weight = st.sidebar.number_input("Enter Empty Weight", min_value = df["empty_weight"].min(), max_value = df["empty_weight"].max(), value = df["empty_weight"].mode().iloc[0], step = 1.0)
+mileage = st.sidebar.number_input("Enter the Mileage", min_value = df["mileage"].min(), max_value = df["mileage"].max(), value = df["mileage"].mode().iloc[0], step = 1.0)
+gears = st.sidebar.number_input("Enter Gears", min_value = df["gears"].min(), max_value = df["gears"].max(), value = df["gears"].mode().iloc[0], step = 1.0)
+cons_avg = st.sidebar.number_input("Enter Consumption", min_value = df["cons_avg"].min(), max_value = df["cons_avg"].max(), value = df["cons_avg"].mode().iloc[0], step = 1.0)
+co_emissions = st.sidebar.number_input("Enter Average CO Emissions", min_value = df["co_emissions"].min(), max_value = df["co_emissions"].max(), value = df["co_emissions"].mode().iloc[0], step = 1.0)
 
 # To load machine learning model
-import pickle
-from sklearn.ensemble import RandomForestRegressor
+
+
 
 file_name = "rf_pipe_model"
+
+
+model_xgb = pickle.load(open("xgb_pipe_model", "rb"))
 model_rf = pickle.load(open(file_name, "rb"))
 
-# model_xgb = pickle.load(open("xgb_pipe_model", "rb"))
 
 my_dict = {"power_kW":power_kw,
            "age":age,
@@ -201,17 +209,18 @@ my_dict = {"power_kW":power_kw,
 
 st.header("The values you selected is below")
 st.markdown("---")
-df = pd.DataFrame.from_dict([my_dict])
-st.table(df)
+# Dictionary'i DataFrame'e çevirme
+df_input = pd.DataFrame.from_dict([my_dict])
 
-import os
-from PIL import Image
+# Sıralama
+df_input = df_input[["make_model", "gearbox", "drivetrain", "power_kW", "age", "empty_weight", "mileage", "gears", "cons_avg", "co_emissions"]]
 
-# Streamlit uygulaması
-st.title("Make Model ve Resim Gösterme")
+# Tabloyu görüntüleme
+st.table(df_input)
 
-# make_model seçim kutusu
-make_model = st.sidebar.selectbox("Select the Auto Brand - Model", df["make_model"].unique(), index=0)
+
+
+st.title("Car Prediction")
 
 # Seçilen make_model'den ilk kelimeyi al ve küçük harfe çevir
 make_model_lower = make_model.split()[0].lower()
@@ -235,7 +244,23 @@ except FileNotFoundError:
         st.warning(f"Resim bulunamadı: {png_image_path} veya {jpg_image_path}")
         st.stop()
 
-# Resmi 400x200 piksel boyutunda göster
-image = image.resize((200, 100))
+# Resmi 60x60 piksel boyutunda göster
+image = image.resize((256, 256))
 
-st.image(image, caption=make_model, use_column_width=True)
+st.image(image, caption=make_model, use_column_width=80)
+# defining the function which will make the prediction using the data
+def prediction(model, input_data):
+	prediction = model.predict(input_data)
+	return prediction
+
+# Making prediction and displaying results
+if st.button("Predict"):
+    if selected_algorithm == "Random Forest":
+        result = prediction(model_rf, df_input)[0]
+    else :
+        result = prediction(model_xgb, df_input)[0]
+
+try:
+    st.success(f"With {selected_algorithm}, Car Price is **{round(result,0)}**")
+except NameError:
+    st.write("Please press **Predict** button to display the result!")
